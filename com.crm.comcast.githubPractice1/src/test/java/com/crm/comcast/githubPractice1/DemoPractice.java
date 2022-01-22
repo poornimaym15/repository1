cf2861a277206deab2983029f8102aa58600493c
USER <USER@Chetan>
2022-01-22 14:36:55 +0530

    first push of framework

---
+++ com.crm.comcast.githubPractice1/src/test/java/com/crm/comcast/githubPractice1/DemoPractice.java
@@ -0,0 +1,10 @@
+package com.crm.comcast.githubPractice1;
+
+public class DemoPractice {
+
+	public static void main(String[] args) {
+		System.out.println("First push of githubPractice");
+
+	}
+
+}
