60cb37e5f5e5b72e62319e314632bd7200e03c28
USER <USER@Chetan>
2022-01-29 17:12:27 +0530

    share to github

--- com.crm.comcast.githubPractice1/src/test/java/com/crm/comcast/githubPractice1/DemoPractice.java
+++ com.crm.comcast.githubPractice1/src/test/java/com/crm/comcast/githubPractice1/DemoPractice.java
@@ -3,7 +3,7 @@ package com.crm.comcast.githubPractice1;
 public class DemoPractice {
 
 	public static void main(String[] args) {
-		System.out.println("First push of githubPractice");
+		System.out.println("Share This Project To GitHub");
 
 	}
 
